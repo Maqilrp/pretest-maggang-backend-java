bf6682acea4d56903d8d516485ac87cfadbcb1b3
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2024-06-20 12:00:12 +0700

    commit

--- ecommerce/src/main/java/com/ecommerce/ecommerce/database/User.java
+++ ecommerce/src/main/java/com/ecommerce/ecommerce/database/User.java
@@ -1,3 +1,9 @@
-public class user {
-    
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String nama;
+    private String email;
+    private String password;
 }
